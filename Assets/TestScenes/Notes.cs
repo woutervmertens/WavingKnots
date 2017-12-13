e936961843605ee6b4e6dc63c8e28dde0ff1fdc0
woutervmertens <woutervmertens@gmail.com>
2017-12-13 15:44:04 +0100

    Sail testscene

---
+++ Assets/TestScenes/Notes.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Notes : MonoBehaviour {
+
+
+
+    /*SAIL
+     * Change Wind effects on cloth with Extrenal Acceleration and Random Acceleration
+     * 
+     * Edit Constraints to hook to frame
+     * 
+     * If needed, add mast collider to Capsule Colliders
+     * 
+     * https://www.youtube.com/watch?v=sbiuwaJ6PT4
+     */
+}
