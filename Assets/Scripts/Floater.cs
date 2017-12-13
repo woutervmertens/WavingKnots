13e56f341eee61ccc7d06372ca3daa1bf233291a
woutervmertens <woutervmertens@gmail.com>
2017-12-13 17:30:59 +0100

    Second Ocean and float version

--- Assets/Scripts/Floater.cs
+++ Assets/Scripts/Floater.cs
@@ -4,9 +4,9 @@ using System.Collections;
 /* -------------------------------------------------------------------------------
 	Floater
  
-	This component works with the Ocean component to make a RigidBody float.
-	When this GameObject moves beneath the Ocean surface it pushes the
-	referenced RigidBody towards the Ocean Normal with an acceleration
+	This component works with the Oceanv1 component to make a RigidBody float.
+	When this GameObject moves beneath the Oceanv1 surface it pushes the
+	referenced RigidBody towards the Oceanv1 Normal with an acceleration
 	based on the depth.
  
 	Tip: Use multiple Floaters spread far apart to make a RigidBody stable.
@@ -17,18 +17,18 @@ public class Floater : MonoBehaviour
     public float LiftAcceleration = 1;
     public float MaxDistance = 3;
     public Rigidbody Body;
-    private Ocean ocean;
+    private Oceanv1 Oceanv1;
 
     void Start()
     {
-        ocean = (Ocean)GameObject.FindObjectOfType(typeof(Ocean));
+        Oceanv1 = (Oceanv1)GameObject.FindObjectOfType(typeof(Oceanv1));
     }
 
     void FixedUpdate()
     {
         Vector3 p = transform.position;
-        float waterHeight = ocean.GetHeightAtLocation(p.x, p.z);
-        Vector3 waterNormal = ocean.GetNormalAtLocation(p.x, p.z);
+        float waterHeight = Oceanv1.GetHeightAtLocation(p.x, p.z);
+        Vector3 waterNormal = Oceanv1.GetNormalAtLocation(p.x, p.z);
         float forceFactor = Mathf.Clamp(1f - (p.y - waterHeight) / MaxDistance, 0, 1);
         transform.parent.GetComponent<Rigidbody>().AddForceAtPosition(waterNormal * forceFactor * LiftAcceleration * Body.mass, p);
 
